3bec7746a164e86f8f905c517b634f056e155714
dibyendubiswas1998 <dibyendubiswas1998@gmail.com>
2022-02-25 17:28:23 +0530

    DB Operation like Create, Drop Database

---
+++ 1. DB_Operations.sql
@@ -0,0 +1,26 @@
+/*
+    DB_Operations:-----------
+*/
+
+/*
+    Creating Database:---
+*/
+create database Tester;
+create database Tester1;
+create database Tester2;
+create database Tester3;
+create database Tester4;
+create database Tester5;
+create database Tester6;
+create database Tester7;
+
+
+
+/*
+    Drop Database:------
+*/
+DROP DATABASE IF EXISTS Tester;
+DROP DATABASE IF EXISTS Tester1, Tester2, Tester3, Tester4, Tester5;
+DROP DATABASE IF EXISTS Tester6, Tester7;
+
+
